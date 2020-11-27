d7517a7abc59f5603c6e53594e34ce2a721095d6
Carlos Filoteo <c.filoteo12@gmail.com>
2020-11-27 00:29:54 -0700

    Mock requests locally

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,39 @@
 import remoteImportMapPlugin from "./index";
 
+const testImportMap = {
+  imports: {
+    react: "https://some.cdn.com/react.js",
+    rxjs: "https://some.cdn.com/rx.js",
+  },
+};
+
+const testDevImportMap = {
+  imports: {
+    react: "https://some.cdn.com/react.dev.js",
+    rxjs: "https://some.cdn.com/rx.dev.js",
+  },
+};
+
+jest.mock("./request", () => {
+  return {
+    __esModule: true,
+    requestJSON: jest.fn((url) => {
+      switch(true) {
+        case url === 'prod': return Promise.resolve(testImportMap)
+        case url === 'dev': return Promise.resolve(testDevImportMap)
+        default: return Promise.reject(
+          "Something went wrong with the provided url to mocked requestJSON",
+        )
+      }
+    }
+    ),
+  };
+});
+
 test("successfully replaces matching file with remote import map urls", async () => {
   const { transform } = remoteImportMapPlugin({}, {
     url: {
-      prod:
-        "https://storage.googleapis.com/react.microfrontends.app/importmap.json",
+      prod: "prod",
     },
   });
 
@@ -24,19 +53,20 @@ import rxjs from 'rxjs';
   const result = transform && await transform(testFile);
 
   expect(result).not.toContain("'react'");
+  expect(result).toContain(testImportMap.imports.react);
   expect(result).not.toContain("'rxjs'");
+  expect(result).toContain(testImportMap.imports.rxjs);
 });
-
 
 test("preserves file with unmatched extension as-is", async () => {
   const { transform } = remoteImportMapPlugin({}, {
     url: {
       prod:
-        "https://storage.googleapis.com/react.microfrontends.app/importmap.json",
+        "prod",
     },
   });
 
-  if(transform) {
+  if (transform) {
     const testFile = {
       id: "testFile",
       contents: `
@@ -47,12 +77,12 @@ import rxjs from 'rxjs';
       isDev: false,
       isHmrEnabled: false,
     };
-  
-    expect(false, )
+
+    expect(false);
     const result = await transform(testFile);
-  
-    expect(result).toEqual(testFile.contents)
+
+    expect(result).toEqual(testFile.contents);
   } else {
-    fail('transform is falsy!')
+    fail("transform is falsy!");
   }
 });
