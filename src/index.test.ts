9a117d07e9eb439674b0747fbacd444dc838a376
Carlos Filoteo <c.filoteo12@gmail.com>
2020-11-27 00:00:30 -0700

    Test unmatched file extension

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,29 +1,58 @@
-import remoteImportMapPlugin from './index'
+import remoteImportMapPlugin from "./index";
 
-const contents = `
-import React from 'react';
-import rxjs from 'rxjs';
-`
-test('snowpack-plugin-remote-import-map', async () => {
-
+test("successfully replaces matching file with remote import map urls", async () => {
   const { transform } = remoteImportMapPlugin({}, {
     url: {
-      prod: "https://storage.googleapis.com/react.microfrontends.app/importmap.json"
-    }
-  })
+      prod:
+        "https://storage.googleapis.com/react.microfrontends.app/importmap.json",
+    },
+  });
 
   expect(transform).toBeTruthy();
 
   const testFile = {
     id: "testFile",
-    contents, 
-    fileExt: ".js", 
+    contents: `
+import React from 'react';
+import rxjs from 'rxjs';
+`,
+    fileExt: ".js",
     isDev: false,
-    isHmrEnabled: false
+    isHmrEnabled: false,
+  };
+
+  const result = transform && await transform(testFile);
+
+  expect(result).not.toContain("'react'");
+  expect(result).not.toContain("'rxjs'");
+});
+
+
+test("preserves file with unmatched extension as-is", async () => {
+  const { transform } = remoteImportMapPlugin({}, {
+    url: {
+      prod:
+        "https://storage.googleapis.com/react.microfrontends.app/importmap.json",
+    },
+  });
+
+  if(transform) {
+    const testFile = {
+      id: "testFile",
+      contents: `
+import React from 'react';
+import rxjs from 'rxjs';
+`,
+      fileExt: ".mjs",
+      isDev: false,
+      isHmrEnabled: false,
+    };
+  
+    expect(false, )
+    const result = await transform(testFile);
+  
+    expect(result).toEqual(testFile.contents)
+  } else {
+    fail('transform is falsy!')
   }
-
-  const result = transform && await transform(testFile)
-
-  expect(result).not.toContain("'react'")
-  expect(result).not.toContain("'rxjs'")
-})
+});
