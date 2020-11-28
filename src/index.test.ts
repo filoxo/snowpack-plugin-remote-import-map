15279a84181e2e977c5c2b82479bf343cbc2d817
Carlos Filoteo <c.filoteo12@gmail.com>
2020-11-28 14:01:49 -0700

    Test that dev urls are used

--- src/index.test.ts
+++ src/index.test.ts
@@ -14,10 +14,17 @@ const testDevImportMap = {
   },
 };
 
-const getTransformResult = async (env: string, testFile: any) => {
+const getTransformResult = async (env: string | [string, string], testFile: any) => {
+  let prod, dev; 
+  if(Array.isArray(env)) {
+    [ prod, dev ] = env;
+  } else {
+    prod = env;
+  }
   const transform = remoteImportMapPlugin(null, {
     url: {
-      prod: env,
+      prod,
+      dev
     },
   }).transform!;
 
@@ -59,6 +66,26 @@ import rxjs from 'rxjs';
   expect(result).toContain(testProdImportMap.imports.react);
   expect(result).not.toContain("'rxjs'");
   expect(result).toContain(testProdImportMap.imports.rxjs);
+});
+
+test("successfully replaces matching file with remote dev import map urls", async () => {
+  const testFile = {
+    id: "testFile",
+    contents: `
+import React from 'react';
+import rxjs from 'rxjs';
+`,
+    fileExt: ".js",
+    isDev: true,
+    isHmrEnabled: false,
+  };
+  const result = await getTransformResult(["prod", "dev"], testFile);
+  expect(result).not.toContain("'react'");
+  expect(result).not.toContain(testProdImportMap.imports.react);
+  expect(result).toContain(testDevImportMap.imports.react);
+  expect(result).not.toContain("'rxjs'");
+  expect(result).not.toContain(testProdImportMap.imports.rxjs);
+  expect(result).toContain(testDevImportMap.imports.rxjs);
 });
 
 test("preserves file with unmatched extension as-is", async () => {
